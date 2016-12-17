0db5fdae45cd37317cc81c134a5c2e5ae22bf0af
Jorge Morazan <jorgemorazan@unitec.edu>
2016-12-17 15:03:22 -0600

    inicio

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <curses.h>
+#include <fstream>
+#include <vector>
+
+int main(){
+	initscr();
+	start_color();
+	char opc[1];
+	vector<Pokemon*>pcBox;
+	do{
+		
+	}while(opc[0]!=);//fin do while
+	endwin();
+}
